72df5452ecd0baf0fcc979160d96c447b742bb21
aidan hopper <aidanhop1@gmail.com>
2022-08-05 22:10:19 -0700

    added Xresources support

--- config.h
+++ config.h
@@ -3,17 +3,20 @@
 
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 static const unsigned int alpha = 0xC0; /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"JetBrainsMono Nerd Fot:size=15"
-};
+static char font[] = "monospace:size=20";
+static const char *fonts[] = { font, "monospace:size=20" };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#FFFFFF", "#2E3440" },
-	[SchemeSel] = { "#434C5E", "#ECEFF4" },
-	[SchemeOut] = { "#2E3440", "#00ffff" },
 
-};
+static char normfgcolor[] = "#bbbbbb";
+static char normbgcolor[] = "#222222";
+static char selfgcolor[]  = "#eeeeee";
+static char selbgcolor[]  = "#005577";
+static char *colors[SchemeLast][2] = {
+ 	/*     fg         bg       */
+	[SchemeNorm] = { normfgcolor, normbgcolor },
+	[SchemeSel]  = { selfgcolor,  selbgcolor  },
+	[SchemeOut]  = { "#000000",   "#00ffff" },
+ };
 
 static const unsigned int alphas[SchemeLast][2] = {
 	[SchemeNorm] = { OPAQUE, alpha },
@@ -33,3 +36,16 @@ static const char worddelimiters[] = " ";
 
 /* Size of the window border */
 static unsigned int border_width = 5;
+
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "dmenufont",        STRING, &font },
+	{ "color7",      STRING, &normfgcolor },
+	{ "color0", STRING, &normbgcolor },
+	{ "color15",  STRING, &selfgcolor },
+	{ "color8",  STRING, &selbgcolor },
+	{ "prompt",      STRING, &prompt },
+};
